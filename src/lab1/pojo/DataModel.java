f7a4e05449580f940a45946884519c1155925aa3
DAUTREY <mikael.dautrey@isitix.com>
2016-05-17 15:24:21 +0200

    restructuration de l objet data model

--- src/lab1/pojo/DataModel.java
+++ src/lab1/pojo/DataModel.java
@@ -1,14 +1,20 @@
 package lab1.pojo;
 
+import java.io.IOException;
+
 /**
  * Created by mdautrey on 17/05/16.
  */
 public class DataModel {
     private String name;
+    DataAccess dataAccess;
+
 
     // constructeur
-    public DataModel(String name){
+    public DataModel(String name, String fileName){
+
         this.name = name;
+        this.dataAccess = DataAccess.getInstanceOf(fileName);
     }
     public String getName(){ return this.name; }
     public void setName(String name){ this.name = name;}
@@ -17,12 +23,38 @@ public class DataModel {
         return getName() + " " + (int)(Math.random() * 100);
     }
 
+    public void writeLines(String fileName, int lines, boolean left){
+
+        String text = "";
+        try {
+            text = dataAccess.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for(int i = 0; i < lines; i++){
+            if(left){
+                text = this.getRandomString() + "\n" +  text;
+            }else{
+                text += this.getRandomString() + "\n";
+            }
+        }
+        try {
+            dataAccess.write(text);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+
 
     public static void main(String args[]){
-        DataModel dm = new DataModel("test");
+        DataModel dm = new DataModel("test", "file.txt");
         for(int i = 0; i < 10; i++){
             System.out.println(dm.getRandomString());
         }
     }
+
 
 }
