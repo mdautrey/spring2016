1a9385b1c25c9fd64700704c44d1c932d82914f1
DAUTREY <mikael.dautrey@isitix.com>
2016-05-17 15:19:44 +0200

    modification position dataAccess

--- src/lab1/pojo/ApplicationNoThread.java
+++ src/lab1/pojo/ApplicationNoThread.java
@@ -14,22 +14,23 @@ public class ApplicationNoThread {
         System.out.println("Entrer le nombre de lignes : ");
         lines = entree.nextInt();
 
-        DataAccess da = DataAccess.getInstanceOf(fileName);
+
 
         // creer un objet DataModel writer 1
         // ecrire 1000 lignes en debut de fichier de type numero ligne = getRandomString
         // chaque nouvelle ligne est au dessus de la precedente
         DataModel writer1 = new DataModel("writer1");
-        ApplicationNoThread.writeLines(writer1, da, lines, true);
+        ApplicationNoThread.writeLines(writer1, fileName, lines, true);
 
 
         // creer un objet DataModel writer 2
         // ecrire 1000 lignes en fin de fichier de type numero ligne = getRandomString
         // chaque nouvelle ligne est en dessous de la precedente
         DataModel writer2 = new DataModel("writer2");
-        ApplicationNoThread.writeLines(writer2, da, lines, false);
+        ApplicationNoThread.writeLines(writer2, fileName, lines, false);
     }
-    public static void writeLines(DataModel writer, DataAccess da, int lines, boolean left){
+    public static void writeLines(DataModel writer, String fileName, int lines, boolean left){
+        DataAccess da = DataAccess.getInstanceOf(fileName);
         String text = "";
         try {
             text = da.read();
